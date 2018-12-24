df95eadced25f7818580c46636d7e6fd006da9fa
Reinier Zwikker <reinier@zwikker.eu>
2018-12-24 14:55:59 +0100

    Removed RF_Functions

--- Main_ScriptieR-AF_v2/Main_ScriptieR-AF_v2.ino
+++ Main_ScriptieR-AF_v2/Main_ScriptieR-AF_v2.ino
@@ -3,24 +3,4 @@
     Annefleur Kirpensteijn & Reinier Zwikker
 
   Verstoppertje spelen met Robots
-
-  TODO:
-  - RF comms
-    - digitalWrite
-    - SoftwareSerial library
-    - RH_ASK library
-  -
-
-
-  
 */
-
-//#include <RH_ASK.h>
-//#include <SPI.h>
-
-//RH_ASK driver;
-
-
-
-
-
